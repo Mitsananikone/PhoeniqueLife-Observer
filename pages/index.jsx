41b837409351cfe23d80262c9cb1ef7d03e03384
Mit Sananikone <Mitsananikone@gmail.com>
2023-07-14 16:24:33 -0500

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,3 @@
-import Link from 'next/link';
-import FlightSearchForm from './account/flightSearchForm';
-import  Nav  from '../components/nav/Nav';
 import { userService } from 'services';
 
 export default Home;
